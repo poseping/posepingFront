eb46658cf2da2547e85355a12484502ab0b6a35f
유진 <ggthereisjin@gmail.com>
2026-04-15 10:00:47 +0900

    chore: 프론트엔드를 Cloud Run 백엔드에 연결

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { getToken, clearAuth } from './authService'
 
-const API_BASE = 'http://localhost:8000/api'
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
 
 const apiClient = axios.create({
   baseURL: API_BASE,
