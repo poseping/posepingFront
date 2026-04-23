4f3f4d5790f6f3edfdae6f38dd6d520f6601729d
유진 <ggthereisjin@gmail.com>
2026-04-23 15:37:31 +0900

    feat: 웹캠 분석기록 차트 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import WebcamStream from '../components/Webcam/WebcamStream'
+import WebcamHistoryStats from '../components/Webcam/WebcamHistoryStats'
 import PageHeader from '../components/PageHeader'
 import '../styles/main-page.css'
 
@@ -12,6 +13,7 @@ function MainPage() {
       <main>
         <div className="main-page-content">
           <WebcamStream isActive={isActive} onToggle={() => setIsActive(!isActive)} />
+          <WebcamHistoryStats />
         </div>
       </main>
     </div>
